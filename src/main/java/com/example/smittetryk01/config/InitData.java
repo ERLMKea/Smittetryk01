873fae869abab6312c5b9e0b121ab39e532940fc
ERLMKea <erlm@kea.dk>
2022-02-17 09:52:13 +0100

    Now with 3 counties

--- src/main/java/com/example/smittetryk01/config/InitData.java
+++ src/main/java/com/example/smittetryk01/config/InitData.java
@@ -25,11 +25,6 @@ public class InitData implements CommandLineRunner {
         roskilde.setHref("http://localhost:8080/county/0259");
         countyRepository.save(roskilde);
 
-        roskilde.setName("Køgexx");
-        roskilde.setCountyCode("0259");
-        roskilde.setHref("http://localhost:8080/county/0259");
-        countyRepository.save(roskilde);
-
         roskilde.setName("København");
         roskilde.setCountyCode("0101");
         roskilde.setHref("http://localhost:8080/county/0101");
