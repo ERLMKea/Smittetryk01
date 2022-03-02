77331f94f563250dded170605c64082fbe19b726
ERLMKea <erlm@kea.dk>
2022-03-02 10:50:04 +0100

    JSonBackReference

--- src/main/java/com/example/smittetryk01/model/Region.java
+++ src/main/java/com/example/smittetryk01/model/Region.java
@@ -1,5 +1,7 @@
 package com.example.smittetryk01.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +17,7 @@ public class Region {
 
     @OneToMany
     @JoinColumn(name="regioncode")
+    @JsonBackReference
     private Set<County> counties = new HashSet<>();
 
     public String getRegionCode() {
