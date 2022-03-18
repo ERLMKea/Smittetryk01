e4e664306b779c76c389b52952e8b76e7ac7fc92
ERLMKea <erlm@kea.dk>
2022-03-18 17:28:44 +0100

    Not necessary to write public in interface

--- src/main/java/com/example/smittetryk01/repository/CountyRepository.java
+++ src/main/java/com/example/smittetryk01/repository/CountyRepository.java
@@ -10,19 +10,19 @@ import java.util.Optional;
 
 public interface CountyRepository extends JpaRepository<County, String> {
 
-    public Optional<County> findByName(String name);
+    Optional<County> findByName(String name);
 
-    public Optional<County> findByHref(String href);
+    Optional<County> findByHref(String href);
 
     //this line will compile, but not run, cannot invoke findByKurt
     //Error creating bean with name 'countyRepository' defined in com.example.smittetryk01.repository.CountyRepository defined in @EnableJpaRepositories declared on JpaRepositoriesRegistrar.EnableJpaRepositoriesConfiguration: Invocation of init method failed; nested exception is org.springframework.data.repository.query.QueryCreationException: Could not create query for public abstract java.util.Optional com.example.smittetryk01.repository.CountyRepository.findByKurt(java.lang.String)! Reason: Failed t
     //public Optional<County> findByKurt(String kurt);
 
-    public List<County> findByNameIsLike(String name);
+    List<County> findByNameIsLike(String name);
 
-    public List<County> findCountyByRegion(Region region);
+    List<County> findCountyByRegion(Region region);
 
-    public List<County> findCountyByRegionRegionCode(String regionCode);
+    List<County> findCountyByRegionRegionCode(String regionCode);
 
     @Query("SELECT COUNT(c) FROM County c WHERE c.name like ?1")
     long countCountyByNameLike(String name);
