ef26f1ab4de040be762591f5f32483552bc0b7b3
MathiasReker <math96r1@stud.kea.dk>
2022-04-06 18:16:48 +0200

    Add @AutoConfigureTestDatabase to test

--- src/test/java/com/example/smittetryk01/Smittetryk01ApplicationTests.java
+++ src/test/java/com/example/smittetryk01/Smittetryk01ApplicationTests.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class Smittetryk01ApplicationTests {
 
