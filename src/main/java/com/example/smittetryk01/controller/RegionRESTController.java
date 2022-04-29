cb528a67a9ac497c4f9bae729f93ce1771f95e2c
ERLMKea <erlm@kea.dk>
2022-04-29 10:45:41 +0200

    Nu med sleep endpoint

--- src/main/java/com/example/smittetryk01/controller/RegionRESTController.java
+++ src/main/java/com/example/smittetryk01/controller/RegionRESTController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -26,6 +27,40 @@ public class RegionRESTController {
     public Region postRegion(@RequestBody Region region) {
         System.out.println(region);
         return regionRepository.save(region);
+    }
+
+    @GetMapping("/region/{id}")
+    public Region getRegion(@PathVariable String id) {
+        Optional<Region> obj = regionRepository.findById(id);
+        if (obj.isPresent()) {
+            return obj.get();
+        }
+        Region region = new Region();
+        region.setName("Not Found");
+        return region;
+    }
+
+    @GetMapping("/regionsleep/{id}/{thrid}")
+    public Region getRegionSleep(@PathVariable String id, @PathVariable String thrid) {
+        Optional<Region> obj = regionRepository.findById(id);
+
+        if (thrid.contains("1") | thrid.contains("3") | thrid.contains("5")) {
+            System.out.println("VI SKAL SOVE Thread =" + thrid);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException err) {
+                System.out.printf("Except=" + err.getMessage());
+            }
+        }
+
+        if (obj.isPresent()) {
+            Region reg = obj.get();
+            reg.setHref(thrid);
+            return reg;
+        }
+        Region region = new Region();
+        region.setName("Not Found");
+        return region;
     }
 
 
